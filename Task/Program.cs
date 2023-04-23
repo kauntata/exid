446920dd093a763bdd1f0e79ebd709f70815f950
unknown <kauntata@mail.ru>
2023-04-23 13:20:20 +0400

    676

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,6 +1,6 @@
 ﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
 string[] array2 = new string[array1.Length];
-
+void SecondArrayWithIF(string[] array1, string[] array2)
 {
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
@@ -12,6 +12,13 @@ string[] array2 = new string[array1.Length];
         }
     }
 }
-
-
-Console.WriteLine();
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
