2ebd97060f6f24d8ab5ce15b53609c6471370c69
unknown <kauntata@mail.ru>
2023-04-23 13:18:11 +0400

    636

---
+++ Task/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array2 = new string[array1.Length];
+
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+
+Console.WriteLine();
